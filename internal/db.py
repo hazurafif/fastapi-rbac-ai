90a55ee1a3e3ccc75c882b4e48ec6d727f0d1db4
Rafif <hanifrafif22@gmail.com>
2025-08-27 15:06:03 +0700

    add migration

--- internal/db.py
+++ internal/db.py
@@ -1,3 +1,21 @@
+from contextlib import contextmanager
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+metadata = Base.metadata
+
+DATABASE_URL = "postgresql+psycopg2://myuser:mypassword@localhost:5432/mydb"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
+
+def get_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+get_db = contextmanager(get_session)
